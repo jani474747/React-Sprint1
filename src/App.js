02eb3112488fc845ede8992ab59834b842a85fd8
jani474747 <96036214+jani474747@users.noreply.github.com>
2022-04-11 13:16:09 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,68 +1,123 @@
+import { useState } from "react";
 
-import './App.css';
-import {useState} from "react"
 function App() {
-let obj = {
-  "Score":60,
-  "Wickets":2,
-  "Overs":5.2
+ 
+  const [score, setScore] = useState(76);
+  const [wicket,setWicket] = useState(2);
+  const [over , setOver] = useState(50)
+
+  if(score> 100){
+    var ans = "India win"
+  }
+  if(wicket==10){
+    var ans = "india loss"
+  }
+  const handle =(val)=>{
+    if(val ===0){
+      setScore(0);
+    }
+    else if(val === 4){
+      setScore(score + 4);
+    }
+    else if(val ===6){
+      setScore(score + 6);
+    }
+    else{
+      if(score + val <0) return;
+      setScore(score + val);
+    }
+  };
+
+  const wickets=(wi)=>{
+    if(wi===0){
+      setWicket(0)
+    }
+    else{
+      if(wicket +wi < 0) return;
+      setWicket(wicket + wi)
+    }
+  }
+
+  const overs =(ov)=>{
+    if(ov ==0){
+      setOver(0)
+    }
+  
+  
+  else{
+    if(over +ov < 0) return;
+    setOver(over+ ov)
+  }
 }
 
-const [Score,setSchore] = useState(obj.Score);
-const [Wickets,setWickets] = useState(obj.Wickets);
-const [Overs,setOvers] = useState(obj.Overs);
-
-
   return (
-<div className="App">
-<header> 
-<h1 className="header">India</h1>
-</header>
-<main>
- <div className="Schore">
- <span>Wickets:</span>
- <h1 className="scoreCount">50</h1>
- </div>
- <div className="Wickets">
- <span>Wickets:</span>
- <h1 className="WicketsCount">2</h1>
- </div>
- <div className="overs">
- <span>Overs:</span>
- <h1 className="WicketsCount">50</h1>
- </div>
- 
- <div className = "buttonsDiv">
- <h1>Add Score</h1>
- <button className="btn1" onclick ={()=>{
-   setScore(Score+1)
- }}>Add 1 </button>
- <button calssName = "btn2" onClick={()=>{
-   setScore(Score+4)
- }}>Add 4</button>
- <button className = "btn3" onClick={()=>{
-   setScore(Score+6)
- }}>Add 6</button>
- </div>
- <div className = "buttonDiv2">
-  <h1>Add Wickets</h1>
- <button className="wicketbtn1" onClick={()=>{
-   setWickets(Wickets+1)
- }} >Add 1</button>
- 
- </div>
- <div className = "Overs">
- <h1>Add Overs</h1>
- <button className="Oversbtn1" onClick={()=>{
-  setOvers(Overs+1)
-}} ></button></div>
- </main>
-</div>
+    <div className="App">
+      <h3>India:</h3>
+      <div className="banner">
+        <div>
+          Score:{" "}
+          <h1 className="scoreCount">
+            {
+              // show "score" here
+              score
+            }
+          </h1>
+        </div>
+        <div>
+          Wicket:{" "}
+          <h1 className="wicketCount">
+            {
+              // show wicket here
+              wicket
+            }
+          </h1>
+        </div>
 
+        <div>
+          Over:{" "}
+          <h1 className="overCount">
+            {
+              // Show Over here in the format: "over.ball" eg: 4.5 means 4th over and 5th ball
+              // if 1 more ball is thrown then over is now 5.0
+              // you have to write logic to form this string from current ball number.
+              over
+            }
+          </h1>
+        </div>
+      </div>
 
+      <div className="addScore">
+        Add Score
+        {/* these buttons should add the respective amount in the score */}
+        <button className="addScore1" onClick={()=>{
+          handle(1);
+        }} >Add 1</button>
+        <button className="addScore4" onClick={()=>{
+          handle(4)
+        }} >Add 4</button>
+        <button className="addScore6" onClick={()=>{
+          handle(6)
+        }} >Add 6</button>
+      </div>
 
-  )}
+      <div className="addWicket">
+        Add Wicket
+        {/* Increase the count of wicket */}
+        
+        <button onClick={()=>{
+          wickets(1)
+        }} >Add 1 wicket</button>
+      </div>  
 
-  //function CricketSchore(itemobj,itemname,classname)
+      <div className="addBall">
+        Add ball
+        {/* Increase the total number of balls thrown here. */}
+        <button>Add 1</button>
+      </div>
+
+      {/* If score reaches greater than 100, show text "India Won" without quotes in h1 tag with class name 'status' */}
+    </div>
+  );
+}
 
 export default App;
